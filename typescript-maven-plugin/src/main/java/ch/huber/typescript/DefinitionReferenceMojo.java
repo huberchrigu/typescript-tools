b2ce9cf1cfa58e10537fec526ec487bda24078a3
huberchrigu <huber.chrigu@gmail.com>
2015-12-28 13:34:04 +0100

    Added maven plugin to run definition reference creation

---
+++ typescript-maven-plugin/src/main/java/ch/huber/typescript/DefinitionReferenceMojo.java
@@ -0,0 +1,31 @@
+package ch.huber.typescript;
+
+import ch.huber.typescript.definitions.DefinitionReferenceCreator;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.nio.file.Path;
+
+/**
+ * Goal which {@link DefinitionReferenceCreator creates references to typescript definition files}.
+ *
+ * @goal create-references
+ * @phase process-sources
+ */
+public class DefinitionReferenceMojo
+        extends AbstractMojo {
+    /**
+     * Location of the typescript sources.
+     *
+     * @parameter expression="${project.build.sourceDirectory}"
+     * @required
+     */
+    private Path typescriptSource;
+
+    public void execute()
+            throws MojoExecutionException {
+        DefinitionReferenceCreator creator = new DefinitionReferenceCreator();
+        getLog().info("Execute typescript definition references for path " + typescriptSource + "...");
+        creator.execute(typescriptSource);
+    }
+}
