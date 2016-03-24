19ff80970acc57c8a1cf9851fd2fcb3dc4f0d10b
christoph.huber <christoph.huber@bit.admin.ch>
2016-03-24 09:49:42 +0100

    Newer maven plugin version

--- typescript-maven-plugin/src/main/java/ch/huber/typescript/DefinitionReferenceMojo.java
+++ typescript-maven-plugin/src/main/java/ch/huber/typescript/DefinitionReferenceMojo.java
@@ -3,23 +3,22 @@ package ch.huber.typescript;
 import ch.huber.typescript.definitions.DefinitionReferenceCreator;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.nio.file.Path;
 
 /**
  * Goal which {@link DefinitionReferenceCreator creates references to typescript definition files}.
- *
- * @goal create-references
- * @phase process-sources
  */
+@Mojo(name = "create-references", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class DefinitionReferenceMojo
         extends AbstractMojo {
     /**
      * Location of the typescript sources.
-     *
-     * @parameter property="${project.build.sourceDirectory}"
-     * @required
      */
+    @Parameter(defaultValue = "${project.build.sourceDirectory}")
     private Path typescriptSource;
 
     public void execute()
